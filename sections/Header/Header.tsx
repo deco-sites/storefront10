85b4cf97aef15919852b03ccc2b35e6011438031
henriiquematheus <104517054+matheush3n@users.noreply.github.com>
2024-07-15 03:38:23 -0300

    fixed footer, newsletter and header

--- sections/Header/Header.tsx
+++ sections/Header/Header.tsx
@@ -98,7 +98,7 @@ const Desktop = ({ navItems, logo, searchbar }: Props) => (
         >
           <div class="flex justify-between w-full">
             <span class="text-primary truncate inset-0">Procure itens...</span>
-            <div class="flex items-center gap-2" style={{ marginLeft: '10px' }}>
+            <div class="flex items-center gap-2" style={{ marginLeft: "10px" }}>
               <svg
                 width="16"
                 height="15"
@@ -106,7 +106,7 @@ const Desktop = ({ navItems, logo, searchbar }: Props) => (
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 class="cursor-pointer"
-                style={{ marginLeft: '10px', strokeWidth: '1' }}
+                style={{ marginLeft: "10px", strokeWidth: "1" }}
               >
                 <path
                   fill-rule="evenodd"
@@ -115,10 +115,7 @@ const Desktop = ({ navItems, logo, searchbar }: Props) => (
                   fill="#131313"
                 />
               </svg>
-              <button
-                
-                aria-label="image search button"
-              >
+              <button aria-label="image search button">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="19px"
@@ -126,7 +123,7 @@ const Desktop = ({ navItems, logo, searchbar }: Props) => (
                   width="18px"
                   fill="#131313"
                 >
-                  <path d="M18 13v7H4V6h5.02c.05-.71.22-1.38.48-2H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-5l-2-2zm-1.5 5h-11l2.75-3.53 1.96 2.36 2.75-3.54L16.5 18zm2.8-9.11c.44-.7.7-1.51.7-2.39C20 4.01 17.99 2 15.5 2S11 4.01 11 6.5s2.01 4.5 4.49 4.5c.88 0 1.7-.26 2.39-.7L21 13.42 22.42 12 19.3 8.89zM15.5 9C14.12 9 13 7.88 13 6.5S14.12 4 15.5 4 18 5.12 18 6.5 16.88 9 15.5 9z"/>
+                  <path d="M18 13v7H4V6h5.02c.05-.71.22-1.38.48-2H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-5l-2-2zm-1.5 5h-11l2.75-3.53 1.96 2.36 2.75-3.54L16.5 18zm2.8-9.11c.44-.7.7-1.51.7-2.39C20 4.01 17.99 2 15.5 2S11 4.01 11 6.5s2.01 4.5 4.49 4.5c.88 0 1.7-.26 2.39-.7L21 13.42 22.42 12 19.3 8.89zM15.5 9C14.12 9 13 7.88 13 6.5S14.12 4 15.5 4 18 5.12 18 6.5 16.88 9 15.5 9z" />
                 </svg>
               </button>
             </div>
@@ -206,8 +203,6 @@ const Desktop = ({ navItems, logo, searchbar }: Props) => (
     </div>
   </div>
 );
-
-
 
 const Mobile = ({ logo, searchbar }: Props) => (
   <>
